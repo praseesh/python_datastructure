a1bb71c01ce0470b7081e289e11bb74e38bf0df1
Praseesh <praseeshprasee@gmail.com>
2024-03-26 12:50:28 +0530

    ifelse

---
+++ If Else.py
@@ -0,0 +1,10 @@
+n = int(input("Enter a number: "))
+
+if n > 0:
+    print("Entered Number is Positive")
+
+elif n ==0:
+    print("Entered Number is Zero")
+
+else:
+    print("Entered number is Negative") 
---
+++ Swapping.py
@@ -0,0 +1,13 @@
+# swapping using temp variable
+
+a = 10
+b = 20
+temp = a
+a = b
+b = temp
+ 
+print ("After Swapping A :", a)
+print ("After Swapping B :", b)
+print ("After Swapping temp :", temp)
+
+
