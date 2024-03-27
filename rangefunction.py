c62ec350c090d5fbe222b64c6a8299f5c2329fe6
Praseesh <praseeshprasee@gmail.com>
2024-03-27 15:27:00 +0530

    range

---
+++ rangefunction.py
@@ -0,0 +1,7 @@
+# Printing 50 to 5 decrementing 5
+
+a = list(range(50,5,-5))
+a = set(range(50,5,-5))
+a = tuple(range(50,5,-5))
+print(a)
+
---
+++ whileloop.py
@@ -0,0 +1,5 @@
+i = 1
+while (i<=5):
+    
+    print(i, end=",")
+    i+=1
