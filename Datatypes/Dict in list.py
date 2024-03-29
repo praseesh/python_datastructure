229cfbbafaa20ac31df2881e2a23f5eeaf6f683c
Praseesh <praseeshprasee@gmail.com>
2024-03-29 23:17:44 +0530

    dict in list

---
+++ Datatypes/Dict in list.py
@@ -0,0 +1,16 @@
+data = [
+    {"Name" : "Ram",
+     "Roll No" : "4543",
+     "Age" : 13,
+     "Phone" : 9456445553,
+     "Course" : "BTec"
+     },
+
+     {"Name" : "Kumar",
+     "Roll No" : "453",
+     "Age" : 17,
+     "Phone" : 9458445553,
+     "Course" : "MTec"
+     }
+]
+print(data[0])
