8f4d89ed3bdcfb732e48242b308cd5c551f0c82a
Praseesh <praseeshprasee@gmail.com>
2024-04-03 22:50:58 +0530

    dictionary

---
+++ Datatypes/dictionary.py
@@ -0,0 +1,21 @@
+# Creating a Dictionary
+
+new_dict1 = {
+    "Rahul" : 1234,
+    "Sooraj" : 4567,
+    "Rohan" : 4321
+}
+
+# Accessing the Values
+
+a = new_dict1["Sooraj"]
+print(a)
+print(new_dict1["Rahul"])
+
+# Modifying the Dictionary
+
+new_dict1["Rohan"] = "New value"
+
+new_dict1["Kumar"] = "Newly added"
+
+print(f"Modified Dictionary: {new_dict1}")
---
+++ new.py
@@ -0,0 +1,13 @@
+# dict1 = {"a":1,"b":2,"c":3}
+
+# for i in dict1:
+#     dict1 = 
+# print(dict1)
+
+
+list1 = [1,2,4,5]
+
+list1.insert(4,3)
+print(list1)
+
+list1.
