4e0a6dd94b4c21fa371a3a10155d39c06d564081
Praseesh <praseeshprasee@gmail.com>
2024-04-05 13:55:39 +0530

    list methods updated

--- Datatypes/List Methods.py
+++ Datatypes/List Methods.py
@@ -1,40 +1,67 @@
+# my_list = [3, 1, 4, 1, 5, 9, 2]
+# # Append a number
+
+# my_list.append(6)
+# print(my_list)
+
+# # Append a List
+# words = ["hello", "world", "python"]
+# my_list.append(words)
+
+# # Append list
+
+# nums = [1,3,5,3]
+# nums.append(my_list)
+# print(f"Nums: {nums}")
+
+
+# # Sorting
+
+# nums.sort()
+# print(f"Sorted: {nums}")
+
+# # Reverse()
+# nums.reverse()
+# print(f"Reverse: {nums}")
+
+# # Range in list
+
+# another_list = list(range(95, 3, -5))
+# print(another_list)
+
+
 my_list = [3, 1, 4, 1, 5, 9, 2]
-# Append a number
 
-my_list.append(6)
-print(my_list)
+# # Index()
 
-# Append a List
-words = ["hello", "world", "python"]
-my_list.append(words)
+# index_of_2 = my_list.index(9)
+# print("Index of 4:", index_of_2)
 
-# Append list
+# Extend()
 
-nums = [1,3,5,3]
-nums.append(my_list)
-print(f"Nums: {nums}")
+my_list.extend([7, 8])
+print("After extend:", my_list)
 
+# Copy()
 
-# Sorting
+copied_list = my_list.copy()
+copied_list.append(5432424242)
+print(f"Copied List: {copied_list}")
 
-nums.sort()
-print(f"Sorted: {nums}")
+# Insert()
 
-# Reverse()
-nums.reverse()
-print(f"Reverse: {nums}")
+my_list.insert(2, "x")
+print(f"After Inserting:{my_list}")
 
-# Range in list
+# Remove()
+my_list.remove(8)
+print("After remove:", my_list)
 
-another_list = list(range(95, 3, -5))
-print(another_list)
+# Pop()
+popped_item = my_list.pop()  # Removes and returns the last item
+print("After pop:", my_list, "(Popped Item:", popped_item, ")")
 
+# Count()
 
-my_list = [3, 1, 4, 1, 5, 9, 2]
-
-# Index()
-
-index_of_2 = my_list.index(9)
-print("Index of 4:", index_of_2)
-
-
+count_of_1 = my_list.count(1)
+print("Count of 1:", count_of_1)
