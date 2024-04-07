90401cae97635fd4645277961166060483b457c4
Praseesh <praseeshprasee@gmail.com>
2024-04-07 17:19:11 +0530

    class and obj

--- OOPS/Class_and_obj.py
+++ OOPS/Class_and_obj.py
@@ -1,5 +1,5 @@
 class DangerBoys:
-    
+    year = 2024
     def __init__(self, name, age, place):
         self.name = name
         self.age = age
@@ -7,13 +7,27 @@ class DangerBoys:
        
     def display(self):
         print(f"Name: {self.name}\n Age: {self.age}\n Place: {self.place}")
+        
+    def add_age(self):
+        self.age = self.age + 1
        
     def relocate(self, place):
         self.place = place
+    @classmethod 
+    def add_year(cls):
+        cls.year = cls.year + 1
+        
        
 person_1 = DangerBoys("John", 34, "Tokyo")
 person_2 = DangerBoys("Jacob", 55, "New York")
-
+person_1.add_age()
+person_2.add_age()
 person_1.display()
 person_1.relocate("Canada")
 person_1.display()
+person_2.display()
+
+DangerBoys.add_year()
+
+person_1.display()
+person_2.display()
