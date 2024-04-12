6a373d4a8032f3eba378dac4ce36fb1de30e8672
Praseesh <praseeshprasee@gmail.com>
2024-04-12 13:18:48 +0530

    unit test

--- OOPS/Method_overloading.py
+++ OOPS/Method_overloading.py
@@ -1,11 +1,14 @@
 class A:
-    def func(self):
+    def func(self, name ,age):
         print("Function in Class A")
+    
+    def func(self, name ,age,adr):
+        print("Function in dsfdfd A")
         
 class B(A):
-    def func(self):
-        super().func()
+    def func(self,name):
+        super().func( name,8)
         print("Function in Class B")
         
 obj = B()
-obj.func()
+obj.func("sas")
--- OOPS/Polymorphism_Exp.py
+++ OOPS/Polymorphism_Exp.py
@@ -2,14 +2,16 @@ class Pets:
     def __init__(self, name, age):
         self.name = name
         self.age = age
-
+    def make_sound(self,sound):
+        print(f"The animal sound is {sound}")
+        
 class Cat(Pets):
     
     def info(self):
         print(f"I am a cat. My name is {self.name}. I am {self.age} years old.")
 
-    def make_sound(self):
-        print("Meow")
+    # def make_sound(self):
+    #     print("Meow")
 
 
 class Dog(Pets):
@@ -17,13 +19,17 @@ class Dog(Pets):
     def info(self):
         print(f"I am a dog. My name is {self.name}. I am {self.age} years old.")
 
-    def make_sound(self):
-        print("Bark")
+    # def make_sound(self):
+    #     print("Bark")
 
 cat1 = Cat("Kitty", 2.5)
 dog1 = Dog("Fluffy", 4)
 
-for animal in (cat1, dog1):
-    animal.make_sound()
-    animal.info()
-    animal.make_sound()
+# for animal in (cat1, dog1):
+cat1.make_sound("grrr")
+cat1.info()
+
+
+dog1.make_sound("BOWWW")
+dog1.info()
+
