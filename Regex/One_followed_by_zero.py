1ed3058f3d209fd41d8da9c4882e558ba7150cfb
Praseesh <praseeshprasee@gmail.com>
2024-04-13 09:08:21 +0530

    message

--- Regex/One_followed_by_zero.py
+++ Regex/One_followed_by_zero.py
@@ -1,13 +1,13 @@
 import re
 
-# pattern = r'10*'
-# print(re.search(pattern,"5108910008"))
+pattern = r'10*'
+print(re.search(pattern,"5108910008"))
 
 # check for the pattern dd-mm-yyyy
 
 # pattern = r'\d{1,2}-\d{1,2}-\d{2,4}'
 # print(re.search(pattern,"44-55-3232"))
-
+ 
 # find the word with exactly 8 words
 
 # pattern = r'\w{8}'
