ca3783a7036c7c5366904c899aaa5df1c8605ce9
Praseesh <praseeshprasee@gmail.com>
2024-05-02 15:07:59 +0530

    generators

---
+++ Generators/gen.py
@@ -0,0 +1,13 @@
+def my_decorator(func):
+    def wrapper():
+        print("Something is happening before the function is called.")
+        func()
+        print("Something is happening after the function is called.")
+    return wrapper
+
+@my_decorator
+def say_hello():
+    print("Hello!")
+
+# Calling the decorated function
+say_hello()
