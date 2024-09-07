2f31d74be780205a5688de07b97d11ca9ff26c78
Praseesh <150511158+praseesh@users.noreply.github.com>
2024-09-07 21:03:55 +0530

    Update dttime.py

--- Datetime/dttime.py
+++ Datetime/dttime.py
@@ -4,3 +4,10 @@ from datetime import date
 todays_date = date.today() 
 
 print("Today's date =", todays_date)
+
+import datetime
+
+x = datetime.datetime.now()
+
+print(x.year)
+print(x.strftime("%A"))
