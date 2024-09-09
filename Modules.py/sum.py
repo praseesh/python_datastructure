a3a6ed5e989e42fd2d1483e5a64c6c958eb5a180
Praseesh <150511158+praseesh@users.noreply.github.com>
2024-09-09 21:29:45 +0530

    Update sum.py

--- Modules.py/sum.py
+++ Modules.py/sum.py
@@ -16,3 +16,26 @@ print(s)
 f = multi(6,6,6,)
 print(f)
 
+def Nmaxelements(list1, N):
+    final_list = []
+ 
+    for i in range(0, N):
+        max1 = 0
+ 
+        for j in range(len(list1)):
+            if list1[j] > max1:
+                max1 = list1[j]
+ 
+        list1.remove(max1)
+        final_list.append(max1)
+ 
+    print(final_list)
+ 
+ 
+# Driver code
+list1 = [2, 6, 41, 85, 0, 3, 7, 6, 10]
+N = 2
+ 
+# Calling the function
+Nmaxelements(list1, N)
+
