58e0e9213885a0573e092f7f9b349c81e6f65d6c
Praseesh <150511158+praseesh@users.noreply.github.com>
2024-09-16 19:43:51 +0530

    Update sum.py

--- Decorators/sum.py
+++ Decorators/sum.py
@@ -11,9 +11,9 @@ print(res)
 # def dec():
 #     return "this is a decorator"
 
-# @dec
-# def check():
-#     return "this is check"
+@dec
+def check():
+    return "this is check"
 
 def dec(func):
     def wrapper():
