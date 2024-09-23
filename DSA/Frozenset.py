2d07145e9ede6e941f72cd38b6651b428abadd1f
Praseesh <150511158+praseesh@users.noreply.github.com>
2024-09-23 19:25:00 +0530

    Update Frozenset.py

---
+++ DSA/Frozenset.py
@@ -0,0 +1,15 @@
+# Same as {"a", "b","c"}
+normal_set = set(["a", "b","c"])
+
+print("Normal Set")
+print(normal_set)
+
+# A frozen set
+frozen_set = frozenset(["e", "f", "g"])
+
+print("\nFrozen Set")
+print(frozen_set)
+
+# Uncommenting below line would cause error as
+# we are trying to add element to a frozen set
+# frozen_set.add("h")
