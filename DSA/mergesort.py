880dfd6cd04bc9de881bb2860e2e4ac1be1aa216
Praseesh <150511158+praseesh@users.noreply.github.com>
2024-10-06 22:00:38 +0530

    mergesort.py

--- DSA/mergesort.py
+++ DSA/mergesort.py
@@ -1,2 +1,10 @@
 def merge_sort(array):
-    mid = len(array)
+    if len(array) <= 1:
+        return array
+    mid = len(array) // 2
+    lefthalf = array[mid:]
+    righthalf = array[:mid]
+    
+    sorted_left = merge_sort(lefthalf)
+    sorted_right = merge_sort(righthalf)
+    
