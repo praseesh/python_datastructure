2f3a828a10bc6bf5402ec6fd9f16c8bcd64abc31
Praseesh <praseeshprasee@gmail.com>
2024-10-07 09:58:08 +0530

    small mistake

--- DSA/mergesort.py
+++ DSA/mergesort.py
@@ -1,12 +1,12 @@
 def merge_sort(array):
-    if len(array) <= 1:
-        return array
-    mid = len(array) // 2
-    lefthalf = array[mid:]
-    righthalf = array[:mid]
-    
-    sorted_left = merge_sort(lefthalf)
-    sorted_right = merge_sort(righthalf)
+   if len(array) <= 1:
+       return array
+   mid = len(array) // 2
+   lefthalf = array[mid:]
+   righthalf = array[:mid]
+   
+   sorted_left = merge_sort(lefthalf)
+   sorted_right = merge_sort(righthalf)
    
    return merge(sorted_left,sorted_right)
     
@@ -15,7 +15,7 @@ def merge(left,right):
    i = 0
    j = 0
    while i < len(left) and j < len(right):
-      if left[i] < right[i]:
+      if left[i] < right[j]:
          result.append(left[i])
          i += 1
       else:
@@ -25,6 +25,11 @@ def merge(left,right):
    result.extend(right[j:])
       
    return result
+
+
+unsortedArr = [5,4,3,2,1,4]
+sorted_array = merge_sort(unsortedArr)
+print("Sorted Array: ",sorted_array)
          
           
       
