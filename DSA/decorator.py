6535d63f79daba9a6d600f4421fcec88c0a5703e
Praseesh <praseeshprasee@gmail.com>
2024-10-08 15:38:14 +0530

    dec

---
+++ DSA/decorator.py
@@ -0,0 +1,12 @@
+def decorator(func):
+    def wrapper():
+        print("Before function call")
+        func()
+        print("After function call")
+    return wrapper
+
+@decorator
+def say_hello():
+    print("Hello!")
+
+say_hello()
