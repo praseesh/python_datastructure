9f158ff6a6e960b782393e4c84f4de8a6275b9f2
Praseesh <praseeshprasee@gmail.com>
2024-10-10 19:58:47 +0530

    reverse string

--- DSA/reverse_string.py
+++ DSA/reverse_string.py
@@ -10,4 +10,16 @@
 
 # Input: s = ["H","a","n","n","a","h"]
 # Output: ["h","a","n","n","a","H"]
- 
+class Solution:
+    def reverseString(self, s) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        left = 0
+        right =  len(s)-1
+        while left < right:
+            s[right], s[left] = s[left], s[right]
+
+            left += 1
+            right -= 1
+        return s
