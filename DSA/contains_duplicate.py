334eefa617691f9fffdee6ce281837c8a3d133c9
Praseesh <praseeshprasee@gmail.com>
2024-10-12 21:12:40 +0530

    contains duplicate

--- DSA/contains_duplicate.py
+++ DSA/contains_duplicate.py
@@ -9,3 +9,14 @@ class Solution:
     
 sol = Solution()
 print(sol.containsDuplicate([1,2,3,1]))
+
+# with less time complexity
+
+class Solution:
+    def containsDuplicate(self, nums) -> bool:
+        dup = set()
+        for i in nums:
+            if i in dup:
+                return True
+            dup.add(i)
+        return False
