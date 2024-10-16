d8abae6bd692325a1bccd0d73c463976e1b3d25b
Praseesh <praseeshprasee@gmail.com>
2024-10-16 21:33:31 +0530

    height checker

--- DSA/1051_height_checker.py
+++ DSA/1051_height_checker.py
@@ -1,7 +1,9 @@
-def heightChecker(self, heights):
+def heightChecker(heights):
     expected = sorted(heights)
     count = 0
     for i in range(len(heights)):
         if heights[i] != expected[i]:
             count += 1
     return count
+heights = [1,1,4,2,1,3]
+print(heightChecker(heights))
