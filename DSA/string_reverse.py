9a3e4168ea3d0ad9760e519f75e71c39191e3fb5
Praseesh <150511158+praseesh@users.noreply.github.com>
2024-10-23 21:17:29 +0530

    Update string_reverse.py

--- DSA/string_reverse.py
+++ DSA/string_reverse.py
@@ -1,27 +1,13 @@
 s = "praseesh"
-
 rev = ""
-
 for ch in s:
-  
- 
     rev = ch + rev
-
 print(rev)
 
 
 s = "GeeksforGeeks"
-
 stack = list(s)
-
-# Initialize an empty string to hold the reversed result
 rev = ""
-
-# Continue looping until the stack is empty
 while stack:
-  
-    # Remove the top element from the stack 
-    # and add it to the reversed string
     rev += stack.pop()
-
 print(rev)
