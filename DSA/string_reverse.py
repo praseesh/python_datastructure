f9300a5bb6a28c1f1d766367a8a5f334a8fd6d48
Praseesh <150511158+praseesh@users.noreply.github.com>
2024-10-23 21:02:09 +0530

    Update string_reverse.py

--- DSA/string_reverse.py
+++ DSA/string_reverse.py
@@ -1,12 +1,10 @@
 s = "praseesh"
 
-# Initialize an empty string to hold reversed result
 rev = ""
 
-# Loop through each character in original string
 for ch in s:
   
-    # Add current character to front of reversed string
+    # Add cto front of reversed string
     rev = ch + rev
 
 print(rev)
