65d629de395d76147fa51e5cbe80ad4490f3165c
Praseesh <praseeshprasee@gmail.com>
2024-10-26 09:52:35 +0530

    done

--- DSA/string_reverse.py
+++ DSA/string_reverse.py
@@ -5,18 +5,15 @@ def reverse(string):
         print(f"i -> {i}\nstr -> {reversed_string}")
     print(reversed_string)
 
-# First function call
 string = "english"
 reverse(string)
 
-# Another example of reversing a string
 s = "praseesh"
 rev = ""
 for ch in s:
     rev = ch + rev
 print(rev)
 
-# Using a stack to reverse a string
 s = "GeeksforGeeks"
 stack = list(s)
 rev = ""
