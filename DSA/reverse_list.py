478bd2319f78233f46bd0442a72b17862cfe7896
Praseesh <praseeshprasee@gmail.com>
2024-11-22 11:16:53 +0530

    reverse list

--- DSA/reverse_list.py
+++ DSA/reverse_list.py
@@ -2,4 +2,10 @@ def reverse_list(arr):
     size = len(arr)
     i = 0
     while i < size // 2:
-        arr[i]
+        arr[i], arr[size - 1 - i] = arr[size - 1 - i], arr[i]
+        i += 1
+    return arr
+        
+arr = [1,2,3,4,5,6]
+reversed = reverse_list(arr)
+print(reversed)
