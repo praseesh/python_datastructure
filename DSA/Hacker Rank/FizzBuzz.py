247d40852d51d83b840361bf7cf5b5ac6fc026e4
Praseesh <praseeshprasee@gmail.com>
2024-11-24 11:21:13 +0530

    FizzBuzz

---
+++ DSA/Hacker Rank/FizzBuzz.py
@@ -0,0 +1,13 @@
+def fizzBuzz(n):
+    for i in range(1, n + 1):  
+        if i % 5 == 0 and i % 3 == 0: 
+            print("FizzBuzz")
+        elif i % 5 == 0:  
+            print("Fizz")
+        elif i % 3 == 0:  
+            print("Buzz")
+        else:  
+            print(i)
+
+
+fizzBuzz(15)
