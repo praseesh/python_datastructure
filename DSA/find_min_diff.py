7e293c15ecf90c8bcc49cb6963a6352fa02bfee2
Praseesh <praseeshprasee@gmail.com>
2024-11-25 22:44:06 +0530

    find minimum difference sorted array

--- DSA/find_min_diff.py
+++ DSA/find_min_diff.py
@@ -1,15 +1,21 @@
 def find_min_diff(arr):
     n = len(arr)
-    print(n)
-    sorted_array =[]
+    # Bubble Sort to sort the array
     for i in range(n):
         for j in range(0, n - i - 1):
-            print(f"{arr[j]}, {j},| {arr[i]}, {i}")
             if arr[j] > arr[j + 1]:
                 arr[j], arr[j + 1] = arr[j + 1], arr[j]
-    print(arr)
-    return arr
-arr = [2,65,3,74,7,77,22,88]
-find_min_diff(arr)
+
+    min_value = float('inf')
+    for i in range(n - 1):
+        diff = arr[i + 1] - arr[i]
+        if diff < min_value:
+            min_value = diff
+
+    return min_value
+
+arr = [2, 65, 3, 74, 7, 77, 22, 88]
+min_value = find_min_diff(arr)
+print(min_value)
     
         
