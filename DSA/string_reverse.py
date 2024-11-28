a227aac90c2569246c437a17ec9cad2ea8cfedaa
Praseesh <praseeshprasee@gmail.com>
2024-11-28 12:31:33 +0530

    str reverse

--- DSA/string_reverse.py
+++ DSA/string_reverse.py
@@ -14,7 +14,17 @@ for ch in s:
     rev = ch + rev
 print(rev)
 
-s = "GeeksforGeeks"
+def reverseString(self, s) -> None:
+    left = 0
+    right =  len(s)-1
+    while left < right:
+        s[right], s[left] = s[left], s[right]
+        left += 1
+        right -= 1
+    print("Reversed String:", s)
+    return s
+
+s = "Programming"
 stack = list(s)
 rev = ""
 while stack:
