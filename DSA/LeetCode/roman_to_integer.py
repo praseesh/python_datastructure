56fee2d11a11818b19789a18835d51248ee0aff5
Praseesh <150511158+praseesh@users.noreply.github.com>
2024-11-29 22:45:13 +0530

    Update roman_to_integer.py

---
+++ DSA/LeetCode/roman_to_integer.py
@@ -0,0 +1,16 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        d = {'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000}
+        
+        sum_ = 0
+        a = 0
+        for c in range(len(s)-1, -1, -1):
+            
+            if d[s[c]] >= a:
+                sum_ += d[s[c]]
+                # print(sum_)
+            else:
+                sum_ -= d[s[c]]
+            a = d[s[c]]            
+            
+        return sum_
