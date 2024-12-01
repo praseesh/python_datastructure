2485150f7e4cde9faa9153c71897b4a4c6805b10
Praseesh <praseeshprasee@gmail.com>
2024-12-01 17:29:53 +0530

    longest palindrome

---
+++ DSA/LeetCode/02_longest_palindrome.py
@@ -0,0 +1,23 @@
+def longestPalindrome(s) :
+    res = ""
+    res_len = 0
+    for i in range(len(s)):
+        l = i
+        r = i
+        while l >= 0 and r < len(s) and s[l] == s[r]:
+            if (r-l+1) > res_len:
+                res = s[l:r+1]
+                res_len = r-l+1
+            l -= 1
+            r += 1
+            
+        
+
+
+
+
+
+
+
+s = "babad"
+longestPalindrome(s)
