91740944de14d3656ae04080d757b4383252457a
Praseesh <praseeshprasee@gmail.com>
2024-12-02 17:50:39 +0530

    longest palindrome

--- DSA/LeetCode/5_longest_palindromic_substing.py
+++ DSA/LeetCode/5_longest_palindromic_substing.py
@@ -3,10 +3,18 @@ def longest_palindromic_substring(s):
         while left >=0 and right < len(s) and s[left] == s[right]:
             left -= 1
             right += 1
-            return s[left+1:right]
+        return s[left+1:right]
         
     longest = ""
     for i in range(len(s)):
         p1 = check_palindrome(i,i)
         p2 = check_palindrome(i,i+1)
         
+        if len(p1) > len(longest):
+            longest = p1
+        if len(p2) > len(longest):
+            longest = p2
+    return longest
+
+s = "cbbd"
+print(longest_palindromic_substring(s))
