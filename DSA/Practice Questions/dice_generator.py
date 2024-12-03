9ffa87e748082bc1f988dd2a2d83bf409c1d72ff
Praseesh <praseeshprasee@gmail.com>
2024-12-03 11:43:07 +0530

    Dice Generator

---
+++ DSA/Practice Questions/dice_generator.py
@@ -0,0 +1,8 @@
+import random
+
+def dice_generator():
+    dice_nums = ["One", "Two", "Three", "Four", "Five", "Six"]
+    dice_number = random.randint(0,6)
+    print(dice_nums[dice_number])
+    
+dice_generator()
