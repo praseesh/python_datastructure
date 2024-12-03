fa47f944ac024816c5104832f9192c0bd0a1d05f
Praseesh <praseeshprasee@gmail.com>
2024-12-03 21:40:46 +0530

    logger file

--- debug/logging_code.py
+++ debug/logging_code.py
@@ -1,6 +1,20 @@
 import logging
 
-logging.basicConfig(level=logging.INFO, filename="log.log", filemode="w")
+logging.basicConfig(level=logging.DEBUG,
+                    filename="log.log",
+                    filemode="w",
+                    format="%(asctime)s - %(levelname)s - %(message)s ",
+                    )
+
+x = 2
+logging.debug(f"here the x is {x}")
+try:
+    a = 2/2
+    logging.info(f"answer is {a}" )
+except ZeroDivisionError as e:
+    logging.error("ZeroDivisionError")
+logging.info(f"2. answer is {a}" )
+
 
 logging.debug("debug")
 logging.info("info")
