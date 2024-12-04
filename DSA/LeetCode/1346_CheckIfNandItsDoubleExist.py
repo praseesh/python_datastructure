632adc0b1298d33f2f93c75d9714ad8993dd5d5f
Praseesh <praseeshprasee@gmail.com>
2024-12-04 22:14:09 +0530

    leetcode 1346

--- DSA/LeetCode/1346_CheckIfNandItsDoubleExist.py
+++ DSA/LeetCode/1346_CheckIfNandItsDoubleExist.py
@@ -1,7 +1,11 @@
 def check_if_exist(arr):
     values = set(arr)
-    print(values)
-    
+    for num in arr:
+        if num*2 in values and num * 2 != num:
+            return True
+        if num == 0 and arr.count(0) > 1:
+            return True
+    return False
     
 arr = [10,2,5,3]
-check_if_exist(arr)
+print(check_if_exist(arr))
