752ed46494f92b637a0499374aef54d1a0d702f5
Praseesh <praseeshprasee@gmail.com>
2024-12-09 21:44:22 +0530

    find a string

---
+++ DSA/Hacker Rank/find_a_string.py
@@ -0,0 +1,13 @@
+def count_substring(string, sub_string):
+    total = 0
+    for i in range(len(string)):
+        if string[i:len(string)].startswith(sub_string):
+            total += 1
+    return total
+
+if __name__ == '__main__':
+    string = input().strip()
+    sub_string = input().strip()
+    
+    count = count_substring(string, sub_string)
+    print(count)
