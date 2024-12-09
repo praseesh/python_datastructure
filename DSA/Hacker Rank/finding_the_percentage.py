ad04270405cae1d6ebc39b51e937d8217bd44c44
Praseesh <praseeshprasee@gmail.com>
2024-12-09 21:47:10 +0530

    finding %

---
+++ DSA/Hacker Rank/finding_the_percentage.py
@@ -0,0 +1,23 @@
+if __name__ == '__main__':
+
+    n = int(input())
+
+    student_marks = {}
+
+    for _ in range(n):
+
+        name, *line = input().split()
+
+        scores = list(map(float, line))
+
+        student_marks[name] = scores
+
+    query_name = input()
+
+    l1 = list(student_marks[query_name]) 
+
+    addition = sum(l1)
+
+    result = addition/len(l1)
+
+    print('%.2f'% result)
