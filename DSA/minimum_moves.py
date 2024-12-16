578357f5d479678a606f94bfcbd43618635a84a0
Praseesh <150511158+praseesh@users.noreply.github.com>
2024-12-16 19:45:12 +0530

    minimum_moves

--- DSA/minimum_moves.py
+++ DSA/minimum_moves.py
@@ -1,1 +1,11 @@
-# minimum_moves.py
+# minimum_moves
+
+class Solution:
+    def minMoves2(self, nums: List[int]) -> int:
+        nums.sort()
+        a = nums[len(nums)//2]
+        # print(nums)
+        count = 0
+        for i in range(len(nums)):
+            count += abs(nums[i] - a)
+        return count
