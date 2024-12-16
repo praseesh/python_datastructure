7f205d0048e5e766e83fbcc29529e96603772a45
Praseesh <150511158+praseesh@users.noreply.github.com>
2024-12-16 19:45:24 +0530

    Update minimum_moves.py

--- DSA/minimum_moves.py
+++ DSA/minimum_moves.py
@@ -4,7 +4,7 @@ class Solution:
     def minMoves2(self, nums: List[int]) -> int:
         nums.sort()
         a = nums[len(nums)//2]
-        # print(nums)
+        print(nums)
         count = 0
         for i in range(len(nums)):
             count += abs(nums[i] - a)
