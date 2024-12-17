e581874901a47d0da9441b3a6693abbb8d6a6dd2
Praseesh <praseeshprasee@gmail.com>
2024-12-17 18:29:37 +0530

    some modification

--- DSA/Hacker Rank/stringSplit.py
+++ DSA/Hacker Rank/stringSplit.py
@@ -3,7 +3,6 @@ def split_and_join(line):
     b = "-".join(a)
     return(b)
 
-if __name__ == '__main__':
-    line = input()
-    result = split_and_join(line)
-    print(result)
+line = input()
+result = split_and_join(line)
+print(result)
