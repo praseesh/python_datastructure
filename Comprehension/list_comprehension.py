4049b42d38da2718aafed01dbf5aa4bf7d6993ae
Praseesh <praseeshprasee@gmail.com>
2024-12-18 20:55:22 +0530

    List Compreshension

--- Comprehension/list_comprehension.py
+++ Comprehension/list_comprehension.py
@@ -6,3 +6,13 @@ for animal in animals:
 print(filtered_animals)
 
 
+filtered_animal = [animal for animal in animals]
+print(filtered_animal)
+
+# a,b,c = [1,2,3]
+
+# print(f"a:{a}\nb:{b}\nc:{c}")
+
+a,_,c = [1,2,3]
+
+print(f"a:{a}\nb:{_}\nc:{c}")
