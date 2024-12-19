539cf9e65d2fd3d5a1b564042e1c51991b8cd11b
Praseesh <praseeshprasee@gmail.com>
2024-12-19 21:19:27 +0530

    Lc

---
+++ DSA/LeetCode/1652_Defuse_the_Bomb.py
@@ -0,0 +1,14 @@
+class Solution:
+    def decrypt(self, code, k):
+        n = len(code)
+        for i in range(n):
+            if k==0:
+                return [0] * n
+            result = [0] * n
+        for i in range(n):
+            if k > 0:
+                result[i] = sum(code[(i + j) % n] for j in range(1, k + 1))
+            elif k < 0:
+                result[i] = sum(code[(i - j) % n] for j in range(1, abs(k) + 1))
+        return result
+            
