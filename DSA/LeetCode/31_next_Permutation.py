6ccb539b1290556a0d9740df0a3fe6b438c420ef
Praseesh <praseeshprasee@gmail.com>
2024-12-29 12:52:53 +0530

    Lc

---
+++ DSA/LeetCode/31_next_Permutation.py
@@ -0,0 +1,24 @@
+class Solution:
+    def nextPermutation(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        n = len(nums)
+        pivot = 0
+
+        for i in range(n - 1, 0, -1):
+            if nums[i - 1] < nums[i]:
+                pivot = i
+                break
+
+        if pivot == 0:
+            nums.sort()
+            return
+
+        swap = n - 1
+        while nums[pivot - 1] >= nums[swap]:
+            swap -= 1
+
+        nums[pivot - 1], nums[swap] = nums[swap], nums[pivot - 1]
+
+        nums[pivot:] = sorted(nums[pivot:])
