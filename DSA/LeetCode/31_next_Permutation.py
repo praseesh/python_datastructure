e9b1d474e5316a5e1de7d152b4783f51ea195dac
Praseesh <praseeshprasee@gmail.com>
2024-12-29 12:54:34 +0530

    Lc

--- DSA/LeetCode/31_next_Permutation.py
+++ DSA/LeetCode/31_next_Permutation.py
@@ -1,21 +1,18 @@
 class Solution:
-    def nextPermutation(self, nums) -> None:
+    def nextPermutation(self, nums):
+        
         n = len(nums)
         pivot = 0
-
         for i in range(n - 1, 0, -1):
             if nums[i - 1] < nums[i]:
                 pivot = i
                 break
-
         if pivot == 0:
             nums.sort()
             return
-
         swap = n - 1
         while nums[pivot - 1] >= nums[swap]:
             swap -= 1
-
         nums[pivot - 1], nums[swap] = nums[swap], nums[pivot - 1]
 
         nums[pivot:] = sorted(nums[pivot:])
