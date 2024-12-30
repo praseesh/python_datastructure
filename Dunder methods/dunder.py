0fa850c888af21cf57a3a559bde3ea3c6fb375df
Praseesh <praseeshprasee@gmail.com>
2024-12-30 09:47:12 +0530

    dunder methods

--- Dunder methods/dunder.py
+++ Dunder methods/dunder.py
@@ -2,5 +2,12 @@ str1 = "hello"
 str2 = " "
 str3 = "world"
 
+# using __str__() method   
+
 new_str = str1.__add__(str2).__add__(str3)
 print(new_str)
+
+
+# using __len__() method    
+
+print(new_str.__len__())
