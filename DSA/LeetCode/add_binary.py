1fc1565df406fe78dd00df792fe7f1b56a0eab64
Praseesh <praseeshprasee@gmail.com>
2025-01-02 20:33:59 +0530

    Add binary

--- DSA/LeetCode/add_binary.py
+++ DSA/LeetCode/add_binary.py
@@ -2,12 +2,10 @@ class Solution:
     def addBinary(self, a: str, b: str) -> str:
         A = int(a, 2)
         B = int(b, 2)
-        
         A = bin(A + B)
         return A[2:]
 
 sol = Solution()
-
 a = "1010"
 b = "1011"
 s = sol.addBinary(a,b)
