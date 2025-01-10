f2df4ae1c553aea1d4331f0d0c5ff86ca6da51d7
Praseesh <praseeshprasee@gmail.com>
2025-01-10 20:24:40 +0530

    Multiply Strings

--- DSA/LeetCode/Multiply_Strings.py
+++ DSA/LeetCode/Multiply_Strings.py
@@ -1,8 +1,8 @@
 class Solution:
-    def multiply(self, num1: str, num2: str) -> str:
-        return str(int(num1)*int(num2))
-num1 = "123"
-num2 = "456"
+    def multiply(self, num1: str, num2: str) :
+        return str(int(num1) * int(num2))
+num1 = "1"
+num2 = "4"
 
 sol =Solution()
 print(sol.multiply(num1,num2))
